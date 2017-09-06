9a5e93769d2c6e8fca3c1bfbf3cb055eb45cbc57
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2017-09-06 17:01:19 +0200

    Finish excerise

--- modules/handlers.js
+++ modules/handlers.js
@@ -4,7 +4,9 @@ var fs = require('fs'),
 exports.welcome = function(request, response) {
     console.log("Rozpoczynam obsługę żądania welcome.");
     fs.readFile('templates/start.html', function(err, html) {
-        response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+        response.writeHead(200, {
+            "Content-Type": "text/html; charset=utf-8"
+        });
         response.write(html);
         response.end();
     });
@@ -15,9 +17,11 @@ exports.upload = function(request, response) {
     var form = new formidable.IncomingForm();
     form.parse(request, function(error, fields, files) {
         fs.renameSync(files.upload.path, "test.png");
-        response.writeHead(200, {"Content-Type": "text/html"});
-        response.write("received image:<br/>");
-        response.write("<img src='/show' />");
+        response.writeHead(200, {
+            "Content-Type": "text/html"
+        });
+        response.write("received image:<br>");
+        response.write("<img src='/show' alt='picture'>");
         response.end();
     });
 }
@@ -30,7 +34,9 @@ exports.error = function(request, response) {
 
 exports.show = function(request, response) {
     fs.readFile("test.png", "binary", function(error, file) {
-        response.writeHead(200, {"Content-Type": "image/png"});
+        response.writeHead(200, {
+            "Content-Type": "image/png"
+        });
         response.write(file, "binary");
         response.end();
     });
